09756ef7842e81dd2a3dd4fd61f2e01a319330f4
michalpokusa <72110769+michalpokusa@users.noreply.github.com>
2023-07-26 02:55:01 +0000

    Handling possible scenarios for files

--- fdw.py
+++ fdw.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from src.argument_parser import FDWArgs, cli_args
 from src.utils import (
     files_from_patterns,
@@ -20,6 +22,20 @@ class FDW:
         for relative_file_path in files:
             self.states.setdefault(relative_file_path, file_state(relative_file_path))
 
+    def _compare_file_states(self, relative_file_path: str) -> bool:
+        self._cached_file_state = file_state(relative_file_path)
+        return self.states.get(relative_file_path) == self._cached_file_state
+
+    def _handle_added_file(self, file_path: str):
+        self.states.setdefault(file_path, file_state(file_path))
+
+    def _handle_modified_file(self, file_path: str):
+        self.states[file_path] = self._cached_file_state
+        self._cached_file_state = None
+
+    def _handle_removed_file(self, file_path: str):
+        self.states.pop(file_path)
+
     def watch_for_changes(self):
         while True:
 
@@ -32,7 +48,21 @@ class FDW:
             ):
                 self.args.delay and sleep(self.args.delay)
 
+                if added:
+                    self._handle_added_file(relative_file_path)
+                    continue
+
+                if present_in_both and not self._compare_file_states(relative_file_path):
+                    self._handle_modified_file(relative_file_path)
+                    continue
+
+                if removed:
+                    self._handle_removed_file(relative_file_path)
+                    continue
+
             sleep(self.args.interval)
+
+
 def main():
     app = FDW(cli_args)
     app.compute_starting_states()
