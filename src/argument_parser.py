ddf00742628b52bd9cfa477389cb648ef718fc8f
michalpokusa <72110769+michalpokusa@users.noreply.github.com>
2023-07-26 00:47:29 +0000

    Added initial custom argument parser

---
+++ src/argument_parser.py
@@ -0,0 +1,35 @@
+from sys import exit
+
+from argparse import (
+    ArgumentParser,
+    Namespace,
+    ONE_OR_MORE
+)
+
+
+class FDWArgumentParser(ArgumentParser):
+    def error(self, message):
+        print(f"Error: {message}\n")
+        self.print_help()
+        exit(1)
+
+
+parser = FDWArgumentParser(
+    prog='fdw',
+    description='...',
+)
+
+# Positional arguments
+parser.add_argument(
+    "patterns",
+    metavar="pattern",
+    help="glob pattern to watch for changes",
+    nargs=ONE_OR_MORE,
+)
+
+
+class FDWArgs(Namespace):
+    patterns: "list[str]"
+
+
+args: FDWArgs = parser.parse_args()
